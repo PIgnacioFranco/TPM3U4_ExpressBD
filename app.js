c9df3f420559bc1b64c1bed2a8c9947d997fe5f8
pignaciofranco <franco.p.ignacio@gmail.com>
2021-02-27 02:37:47 -0300

    completando

--- app.js
+++ app.js
@@ -4,13 +4,25 @@ const mysql = require ('mysql');
 const util = require ('util');
 
 const app = express ();
+const puerto = 3000;
 
+app.use (express.json()); // mapeo de json a obj js
 
+const conexion = mysql.createConnection ({
+    host: localhost,
+    user: 'root',
+    password: '',
+    database: ''
+    // completar
+});
 
+conexion.connect();
 
+// Para trabajar con async-await
+const qy = ;
 
 
 // Servidor 
-app.listen (3000, () => {
+app.listen (puerto, () => {
     console.log('Servidor funcionando por puerto 3000!');
 });
